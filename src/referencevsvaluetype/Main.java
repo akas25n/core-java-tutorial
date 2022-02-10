16e51657f8bf7f3cbf9db4a4dbb3a7984189b4dc
Abdur Rahim <akas25n@gmail.com>
2022-02-10 22:52:32 +0100

    initial commit

---
+++ src/referencevsvaluetype/Main.java
@@ -0,0 +1,20 @@
+package referencevsvaluetype;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // reference type
+        int[] myIntArray = new int[5];
+        int[] anotherIntArray = myIntArray;
+
+        System.out.println("My int array: " + Arrays.toString(myIntArray));
+        System.out.println("Another int array: " + Arrays.toString(anotherIntArray));
+
+        anotherIntArray[0] = 5;
+
+        System.out.println("After change My int array: " + Arrays.toString(myIntArray));
+        System.out.println("After change another int array: " + Arrays.toString(anotherIntArray));
+    }
+}
